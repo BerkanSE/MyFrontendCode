1a550513292f4a9c331ea8efd5379a2bb3b7f6b7
BerkanSe <sengerberkann@gmail.com>
2021-04-03 01:12:17 +0300

    3 Nisan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+//Component Declaration
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyReCapProject';
+  title:string = 'MyReCapProject';
+  //Tek tırnak veya çift tırnak farketmez
+
 }
+//Datayı yönettiğimiz yer
