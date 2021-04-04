18e88aae8d8ed9a99650c10e051d2722bfb3315f
BerkanSe <sengerberkann@gmail.com>
2021-04-04 19:32:46 +0300

    4 Nisan

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Car } from 'src/app/models/car';
+
+
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-car',
@@ -7,10 +11,37 @@ import { Car } from 'src/app/models/car';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
+  
   cars:Car[] = [];
-  constructor() { }
+  dataLoaded = false;
+  
+  constructor(private carService:CarService,
+     private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["brandId"]){
+        this.getCarsByBrand(params["brandId"])
+      }else{
+        this.getCars()
+      }
+    })
+
+  }
+
+  getCars(){
+    this.carService.getCars().subscribe(response=>{
+      this.cars = response.data
+      this.dataLoaded = true;
+    })
+  }
+
+  getCarsByBrand(brandId:number){
+    this.carService.getCarsByBrand(brandId).subscribe(response=>{
+      this.cars = response.data
+      this.dataLoaded = true;
+    })
   }
 
 }
